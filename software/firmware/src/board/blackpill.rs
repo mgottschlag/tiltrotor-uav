d7a587b7fd0ea7c62002d5d98448d20bd34413b0
Maximilian Hess <mail@ne0h.de>
2021-08-12 13:43:52 +0200

    Implement radio via nrf24l01

--- software/firmware/src/board/blackpill.rs
+++ software/firmware/src/board/blackpill.rs
@@ -1,12 +1,27 @@
+use stm32f4xx_hal::gpio::gpioa::{PA10, PA5, PA6, PA7, PA8, PA9};
+use stm32f4xx_hal::gpio::{Alternate, AF5};
+use stm32f4xx_hal::gpio::{Output, PushPull};
+use stm32f4xx_hal::pac::SPI1;
 use stm32f4xx_hal::prelude::*;
 use stm32f4xx_hal::pwm;
 use stm32f4xx_hal::pwm::{PwmChannels, C1, C2, C3, C4};
+use stm32f4xx_hal::spi::{Mode, Phase, Polarity, Spi};
 pub use stm32f4xx_hal::stm32 as pac;
 
 use super::EnginePwm;
+use super::Radio;
+
+pub type RadioSck = PA5<Alternate<AF5>>;
+pub type RadioMiso = PA6<Alternate<AF5>>;
+pub type RadioMosi = PA7<Alternate<AF5>>;
+pub type RadioCs = PA8<Output<PushPull>>;
+pub type RadioCe = PA9<Output<PushPull>>;
+pub type RadioIrq = PA10<Output<PushPull>>;
+pub type RadioSpi = Spi<SPI1, (RadioSck, RadioMiso, RadioMosi)>;
 
 pub struct Board {
     pub engines: BlackpillEnginePwm,
+    pub radio: Radio,
 }
 
 impl Board {
@@ -21,16 +36,36 @@ impl Board {
             .freeze();
 
         let gpioa = device.GPIOA.split();
+
+        // init pwm
         let c1 = gpioa.pa0.into_alternate_af2();
         let c2 = gpioa.pa1.into_alternate_af2();
         let c3 = gpioa.pa2.into_alternate_af2();
         let c4 = gpioa.pa3.into_alternate_af2();
         let pins = (c1, c2, c3, c4);
-
         let pwm = pwm::tim5(device.TIM5, pins, clocks, 50.hz());
-
         let engines = BlackpillEnginePwm::init(pwm);
-        Board { engines }
+
+        // init radio
+        let radio_sck = gpioa.pa5.into_alternate_af5();
+        let radio_miso = gpioa.pa6.into_alternate_af5();
+        let radio_mosi = gpioa.pa7.into_alternate_af5();
+        let radio_cs = gpioa.pa8.into_push_pull_output();
+        let radio_ce = gpioa.pa9.into_push_pull_output();
+        let radio_irq = gpioa.pa10.into_push_pull_output();
+        let radio_spi = Spi::spi1(
+            device.SPI1,
+            (radio_sck, radio_miso, radio_mosi),
+            Mode {
+                polarity: Polarity::IdleLow,
+                phase: Phase::CaptureOnFirstTransition,
+            },
+            stm32f4xx_hal::time::KiloHertz(2000).into(),
+            clocks,
+        );
+        let radio = Radio::init(radio_spi, radio_cs, radio_ce, radio_irq);
+
+        Board { engines, radio }
     }
 }
 
