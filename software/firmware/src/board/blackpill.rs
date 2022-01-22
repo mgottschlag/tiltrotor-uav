5bdc26729581c0e831f45d29627e790257228425
Maximilian Hess <mail@ne0h.de>
2022-01-22 20:31:13 +0100

    blackpill

--- software/firmware/src/board/blackpill.rs
+++ software/firmware/src/board/blackpill.rs
@@ -1,20 +1,21 @@
 use stm32f4xx_hal::gpio::gpioa::{PA10, PA5, PA6, PA7, PA8, PA9};
-use stm32f4xx_hal::gpio::{Alternate, Output, PushPull, AF5};
+use stm32f4xx_hal::gpio::{Alternate, Output, PushPull};
+pub use stm32f4xx_hal::pac;
 use stm32f4xx_hal::pac::SPI1;
 use stm32f4xx_hal::prelude::*;
-use stm32f4xx_hal::pwm::{self, PwmChannels, C1, C2, C3, C4};
-use stm32f4xx_hal::spi::{Mode, Phase, Polarity, Spi};
-pub use stm32f4xx_hal::stm32 as pac;
+use stm32f4xx_hal::pwm::{PwmChannel, C1, C2, C3, C4};
+use stm32f4xx_hal::spi::{Mode, Phase, Polarity, Spi, TransferModeNormal};
+use stm32f4xx_hal::timer::Timer;
 
 use super::EnginePwm;
 
-pub type RadioSck = PA5<Alternate<AF5>>;
-pub type RadioMiso = PA6<Alternate<AF5>>;
-pub type RadioMosi = PA7<Alternate<AF5>>;
+pub type RadioSck = PA5<Alternate<PushPull, 5>>;
+pub type RadioMiso = PA6<Alternate<PushPull, 5>>;
+pub type RadioMosi = PA7<Alternate<PushPull, 5>>;
 pub type RadioCs = PA8<Output<PushPull>>;
 pub type RadioCe = PA9<Output<PushPull>>;
 pub type RadioIrq = PA10<Output<PushPull>>;
-pub type RadioSpi = Spi<SPI1, (RadioSck, RadioMiso, RadioMosi)>;
+pub type RadioSpi = Spi<SPI1, (RadioSck, RadioMiso, RadioMosi), TransferModeNormal>;
 
 pub struct Board {
     pub engines: BlackpillEnginePwm,
@@ -38,30 +39,31 @@ impl Board {
         let gpioa = device.GPIOA.split();
 
         // init pwm
-        let c1 = gpioa.pa0.into_alternate_af2();
-        let c2 = gpioa.pa1.into_alternate_af2();
-        let c3 = gpioa.pa2.into_alternate_af2();
-        let c4 = gpioa.pa3.into_alternate_af2();
+        let c1 = gpioa.pa0.into_alternate();
+        let c2 = gpioa.pa1.into_alternate();
+        let c3 = gpioa.pa2.into_alternate();
+        let c4 = gpioa.pa3.into_alternate();
         let pins = (c1, c2, c3, c4);
-        let pwm = pwm::tim5(device.TIM5, pins, clocks, 50.hz());
+        //let pwm = pwm::tim5(device.TIM5, pins, clocks, 50.hz());
+        let pwm = Timer::new(device.TIM5, &clocks).pwm(pins, 50.hz());
         let engines = BlackpillEnginePwm::init(pwm);
 
         // init radio
-        let radio_sck = gpioa.pa5.into_alternate_af5();
-        let radio_miso = gpioa.pa6.into_alternate_af5();
-        let radio_mosi = gpioa.pa7.into_alternate_af5();
+        let radio_sck = gpioa.pa5.into_alternate();
+        let radio_miso = gpioa.pa6.into_alternate();
+        let radio_mosi = gpioa.pa7.into_alternate();
         let radio_cs = gpioa.pa8.into_push_pull_output();
         let radio_ce = gpioa.pa9.into_push_pull_output();
         let radio_irq = gpioa.pa10.into_push_pull_output();
-        let radio_spi = Spi::spi1(
+        let radio_spi = Spi::new(
             device.SPI1,
             (radio_sck, radio_miso, radio_mosi),
             Mode {
                 polarity: Polarity::IdleLow,
                 phase: Phase::CaptureOnFirstTransition,
             },
-            stm32f4xx_hal::time::KiloHertz(2000).into(),
-            clocks,
+            2_000_000.hz(),
+            &clocks,
         );
 
         Board {
@@ -78,20 +80,20 @@ pub type EnginePwmType = BlackpillEnginePwm;
 
 pub struct BlackpillEnginePwm {
     c: (
-        PwmChannels<pac::TIM5, C1>,
-        PwmChannels<pac::TIM5, C2>,
-        PwmChannels<pac::TIM5, C3>,
-        PwmChannels<pac::TIM5, C4>,
+        PwmChannel<pac::TIM5, C1>,
+        PwmChannel<pac::TIM5, C2>,
+        PwmChannel<pac::TIM5, C3>,
+        PwmChannel<pac::TIM5, C4>,
     ),
 }
 
 impl BlackpillEnginePwm {
     pub fn init(
         mut c: (
-            PwmChannels<pac::TIM5, C1>,
-            PwmChannels<pac::TIM5, C2>,
-            PwmChannels<pac::TIM5, C3>,
-            PwmChannels<pac::TIM5, C4>,
+            PwmChannel<pac::TIM5, C1>,
+            PwmChannel<pac::TIM5, C2>,
+            PwmChannel<pac::TIM5, C3>,
+            PwmChannel<pac::TIM5, C4>,
         ),
     ) -> Self {
         c.0.set_duty(0);
