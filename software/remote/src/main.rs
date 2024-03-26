a0dfe58722cf86aab36b74c554ee33de5d367eef
Maximilian Hess <mail@ne0h.de>
2024-03-26 20:41:19 +0100

    remote: introduce offline mode

--- software/remote/src/main.rs
+++ software/remote/src/main.rs
@@ -21,6 +21,9 @@ struct Opts {
         default_value = "/dev/ttyUSB_nrf24l01"
     )]
     device: PathBuf,
+
+    #[structopt(short = "o", long)]
+    offline: bool,
 }
 
 #[tokio::main]
@@ -29,24 +32,16 @@ async fn main() {
     println!("Opts: {opts:?}");
 
     // init command channel
-    let (cmd_tx, cmd_rx) = mpsc::channel::<Command>(32);
-
-    // init radio and and send stop command
-    let mut radio = Radio::new(opts.device, cmd_rx).await;
-    cmd_tx
-        .send(Command {
-            thrust: [0; 4],
-            pose: [0; 2],
-        })
-        .await
-        .unwrap();
+    let (cmd_tx, mut cmd_rx) = mpsc::channel::<Command>(32);
 
     match Gamepad::init() {
         None => {
             println!("Did not find any gamepads - falling back to keyboard");
-            enable_raw_mode().unwrap();
-            keyboard::run(&cmd_tx).await;
-            disable_raw_mode().unwrap();
+            tokio::spawn(async move {
+                enable_raw_mode().unwrap();
+                keyboard::run(&cmd_tx).await;
+                disable_raw_mode().unwrap();
+            });
         }
         Some(mut gamepad) => {
             println!("Found at least one gamepad - waiting for input");
@@ -54,6 +49,19 @@ async fn main() {
         }
     };
 
-    println!("Waiting for commands ...");
-    radio.run().await;
+    match opts.offline {
+        true => {
+            println!("Waiting for commands in offline mode ...");
+            loop {
+                let cmd = cmd_rx.recv().await.unwrap();
+                println!("Got {cmd:?}");
+            }
+        }
+        false => {
+            // init radio
+            let mut radio = Radio::new(opts.device, cmd_rx).await;
+            println!("Waiting for commands ...");
+            radio.run().await;
+        }
+    }
 }
