c7f235df7026f7340b08337c2e4331ad4ced48aa
Maximilian Heß <mail@ne0h.de>
2024-05-24 10:06:20 +0200

    firmware: add error handling for display (#17)

--- software/firmware/src/board/mod.rs
+++ software/firmware/src/board/mod.rs
@@ -18,8 +18,6 @@ mod feather_nrf52840;
 #[cfg(feature = "flightcontroller")]
 mod flightcontroller;
 
-use protocol::Command;
-
 pub trait EnginePwm {
     fn update(&mut self, motor_left: Direction, motor_right: Direction);
 }
--- software/firmware/src/display.rs
+++ software/firmware/src/display.rs
@@ -1,4 +1,5 @@
 use core::fmt::Write;
+use defmt::error;
 use embassy_sync::blocking_mutex::raw::CriticalSectionRawMutex;
 use embassy_sync::channel::Channel;
 use embedded_graphics::{
@@ -9,7 +10,7 @@ use embedded_graphics::{
     text::{Baseline, Text},
 };
 use heapless::String;
-use ssd1306::{prelude::*, I2CDisplayInterface, Ssd1306};
+use ssd1306::{mode::BufferedGraphicsMode, prelude::*, I2CDisplayInterface, Ssd1306};
 
 use crate::board::DisplayI2c;
 
@@ -29,15 +30,31 @@ pub enum Event {
     Battery(f32),
 }
 
-#[embassy_executor::task]
-pub async fn run(i2c: DisplayI2c, event_channel: &'static EventChannel) {
-    let interface = I2CDisplayInterface::new(i2c);
-    let mut display = Ssd1306::new(interface, DisplaySize128x64, DisplayRotation::Rotate0)
-        .into_buffered_graphics_mode();
-    display.init().unwrap();
+#[derive(thiserror_no_std::Error, Debug, defmt::Format)]
+pub enum Error {
+    #[error("interface error")]
+    Interface(#[from] display_interface::DisplayError),
+}
 
-    loop {
-        let event = event_channel.receive().await;
+pub struct Display {
+    display: Ssd1306<
+        I2CInterface<DisplayI2c>,
+        DisplaySize128x64,
+        BufferedGraphicsMode<DisplaySize128x64>,
+    >,
+}
+
+impl Display {
+    pub fn init(i2c: DisplayI2c) -> Result<Self, Error> {
+        let interface = I2CDisplayInterface::new(i2c);
+        let mut display = Ssd1306::new(interface, DisplaySize128x64, DisplayRotation::Rotate0)
+            .into_buffered_graphics_mode();
+        display.init()?;
+
+        Ok(Display { display })
+    }
+
+    pub fn handle(&mut self, event: Event) -> Result<(), Error> {
         let mut msg: String<16> = String::new();
         let y = match event {
             Event::Command(cmd) => {
@@ -47,23 +64,33 @@ pub async fn run(i2c: DisplayI2c, event_channel: &'static EventChannel) {
                     round(cmd.pose[0]),
                     round(cmd.pose[1])
                 )
-                .unwrap();
+                .ok();
                 0
             }
             Event::Battery(voltage) => {
-                write!(&mut msg, "Bat:  {} V", round(voltage)).unwrap();
+                write!(&mut msg, "Bat:  {} V", round(voltage)).ok();
                 20
             }
         };
 
         Rectangle::new(Point::new(0, y), Size::new(128, 10))
             .into_styled(CLEAR_STYLE)
-            .draw(&mut display)
-            .unwrap();
+            .draw(&mut self.display)?;
         Text::with_baseline(msg.as_str(), Point::new(0, y), TEXT_STYLE, Baseline::Top)
-            .draw(&mut display)
-            .unwrap();
-        display.flush().unwrap();
+            .draw(&mut self.display)?;
+        self.display.flush()?;
+
+        Ok(())
+    }
+}
+
+#[embassy_executor::task]
+pub async fn run(mut display: Display, event_channel: &'static EventChannel) {
+    loop {
+        let event = event_channel.receive().await;
+        if let Err(err) = display.handle(event) {
+            error!("Failed to display event: {:?}", err)
+        }
     }
 }
 
